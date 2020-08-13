7e47472b37c4165639f43e49ae5cd592d1eeef78
dsymonov <dsymonov@justcoded.co>
2020-08-13 17:23:46 +0300

    delete vendors js files

--- vendor_entries/vendor.js
+++
@@ -1,6 +0,0 @@
-// All imports in this file will be compiled into vendors.js file.
-
-module.exports = [
-  // example for connect your vendor plugin
-  // 'node_modules/your-plugin/your-plugin.js'
-];
